dc76be5e0353a3a90584b1d7de02dac52560ef76
gabs712 <gabs712.g@gmail.com>
2024-04-17 22:25:10 -0300

    Implement check winner method

---
+++ script.js
@@ -0,0 +1,53 @@
+const GameBoard = (() => {
+  
+})()
+
+const Game = (() => {
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  const checkWinner = (board) => {
+    for (condition of winConditions){
+      if (
+        board[condition[0]] === 'x' &&
+        board[condition[1]] === 'x' &&
+        board[condition[2]] === 'x'
+      ) {
+        console.log('X wins')
+        return 'x'
+      }
+      if (
+        board[condition[0]] === 'o' &&
+        board[condition[1]] === 'o' &&
+        board[condition[2]] === 'o'
+      ) {
+        console.log('O wins')
+        return 'o'
+      }
+    }
+    if (!board.includes('')) {
+      console.log('Tie!')
+      return 'tie'
+    }
+    console.log('not finished')
+    return false
+  }
+
+  return {checkEnd: checkWinner}
+})()
+
+const array = [
+  'x', 'o', 'o',
+  'x', 'o', 'o',
+  'x', 'o', 'x'
+]
+
+Game.checkEnd(array)
