7e46f833e9c0419027bce66d07a09444b6dafba9
gabs712 <gabs712.g@gmail.com>
2024-04-19 00:20:41 -0300

    Add interface changes when the game is started

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ const game = (() => {
     [2, 4, 6],
   ]
 
+  const players = []
+
   const checkWinner = (board) => {
     for (condition of winConditions){
       if (
@@ -63,30 +65,47 @@ const game = (() => {
     return false
   }
 
-  return {checkWinner}
+  return {players, checkWinner}
 })()
 
 
-const Player = (name, mark) => {
+const Player = (name) => {
   let score = 0
 
   const getScore = () => score
   const upScore = () => ++score
 
-  return {name, mark, getScore, upScore}
+  return {name, getScore, upScore}
 }
 
 
-;(function uiEffects() {
-  ;(function inputClear() {
-    const inputs = document.querySelectorAll('input')
-    
-    inputs.forEach((input) => {
-      input.addEventListener('focus', (ev) => {
-        ev.target.value = ''
-      }, {once:true})
-    })
-  })()
+const ui = (() => {
+  const inputs = document.querySelectorAll('[data-input]')
+  const startButton = document.querySelector('[data-start-button]')
+  const labels = document.querySelectorAll('[data-label')
+  const gridCells = document.querySelectorAll('[data-gi]')
 
+  inputs.forEach((input) => {
+    input.addEventListener('focus', (ev) => {
+      ev.target.value = ''
+    }, {once: true})
+  })
+
+  startButton.addEventListener('click', (ev) => {
+    ev.target.remove()
+    inputs.forEach((input) => {
+      game.players.push(Player(input.value))
+      input.disabled = true
+    })
+    labels.forEach((label, i) => {
+      const score = game.players[i].getScore()
+      label.textContent = `Score: ${score}`
+    })
+    gridCells.forEach((cell) => {
+      cell.addEventListener('mouseenter', () => cell.style.opacity = '.6')
+      cell.addEventListener('mouseleave', () => cell.style.opacity = '1')
+    })
+    
+  })
 
 })()
