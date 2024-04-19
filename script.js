a1a97858a27d217f70d7e70279040ced9278eac2
gabs712 <gabs712.g@gmail.com>
2024-04-19 03:15:47 -0300

    Imlpement most of playturn logic

--- script.js
+++ script.js
@@ -5,22 +5,26 @@ const gameBoard = (() => {
     '', '', ''
   ]
 
+  let turn = 0
+
   const reset = () => {
     for (let i = 0; i < board.length; i++) {
       board[i] = ''
     }
+    turn = 0
   }
 
-  const set = (index, mark) => {
-    if (board[index] !== '') {
-      console.log('Cell already occupied')
-      return false
-    }
-    board[index] = mark
-    return true
+  const getTurn = () => turn
+  const upTurn = () => ++turn
+
+  const cellIsEmpty = (index) => {
+    if (board[index] === '') return true
+    return false
   }
 
-  return {board, reset, set}
+  const setBoard = (index, mark) => board[index] = mark
+
+  return {board, reset, setBoard, getTurn, upTurn, cellIsEmpty}
 })()
 
 
@@ -38,14 +42,14 @@ const game = (() => {
 
   const players = []
 
-  const checkWinner = (board) => {
+  const winner = () => {
+    const board = gameBoard.board
     for (condition of winConditions){
       if (
         board[condition[0]] === 'x' &&
         board[condition[1]] === 'x' &&
         board[condition[2]] === 'x'
       ) {
-        console.log('X wins')
         return 'x'
       }
       if (
@@ -53,7 +57,6 @@ const game = (() => {
         board[condition[1]] === 'o' &&
         board[condition[2]] === 'o'
       ) {
-        console.log('O wins')
         return 'o'
       }
     }
@@ -61,11 +64,10 @@ const game = (() => {
       console.log('Tie!')
       return 'tie'
     }
-    console.log('not finished')
     return false
   }
 
-  return {players, checkWinner}
+  return {players, winner}
 })()
 
 
@@ -91,6 +93,32 @@ const ui = (() => {
     }, {once: true})
   })
 
+  const playTurn = (ev) => {
+    const cellIndex = ev.target.dataset.index
+    if (!gameBoard.cellIsEmpty(cellIndex)) return
+
+    // O's turn
+    if (gameBoard.getTurn() % 2 === 0) {
+      gameBoard.setBoard(cellIndex, 'o')
+      ev.target.innerHTML = `<svg class="o" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle-outline</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>`
+    } else { // X's turn
+      gameBoard.setBoard(cellIndex, 'x')
+      ev.target.innerHTML = `<svg class="x" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>`
+    } 
+    gameBoard.upTurn()
+
+    const winner = game.winner()
+    if (!winner) return
+
+    winner === 'o' ? game.players[0].upScore() : game.players[1].upScore()
+    gameBoard.reset()
+    ev.target.removeEventListener('click', playTurn)
+
+    gridCells.forEach((cell) => {
+      cell.removeEventListener('click', playTurn)
+    })
+  }
+  
   startButton.addEventListener('click', (ev) => {
     ev.target.remove()
     inputs.forEach((input) => {
@@ -102,10 +130,10 @@ const ui = (() => {
       label.textContent = `Score: ${score}`
     })
     gridCells.forEach((cell) => {
-      cell.addEventListener('mouseenter', () => cell.style.opacity = '.6')
-      cell.addEventListener('mouseleave', () => cell.style.opacity = '1')
+      cell.style.cursor = 'pointer'
+      cell.addEventListener('mouseenter', (ev) => ev.target.style.opacity = '.6')
+      cell.addEventListener('mouseleave', (ev) => ev.target.style.opacity = '1')
+      cell.addEventListener('click', playTurn)
     })
-    
   })
-
 })()
